fb1aa1bb9e22dbe8deeb9f8f3474f47c3c8a8f7d
tywu999 <tszyengw2026@student.cis.edu.hk>
2024-01-31 11:40:12 +0800

    k

--- src/ClassSection.java
+++ src/ClassSection.java
@@ -1,5 +1,54 @@
-
+import java.util.*;
 public class ClassSection {
+    private String subject;
+    private int capacity;
+    private int yearLevel;
+    private ArrayList<Student> students;
+    public ClassSection(String subject, int capacity, int yearLevel){
+        this.subject = subject;
+        this.capacity = capacity;
+        this.yearLevel = yearLevel;
+        this.students = new ArrayList<>();
+    }
+    public String getSubject(){
+        return subject;
+    }
+    public void setSubject(String subject){
+        this.subject = subject;
+    }
+    public int getCapacity(){
+        return capacity;
+    }
+    public void setCapacity(int capacity){
+        this.capacity = capacity;
+    }
+    public int getYearLevel(){
+        return yearLevel;
+    }
+    public void setYearLevel(int yearLevel){
+        this.yearLevel = yearLevel;
+    }
+    public ArrayList<Student> getStudents(){
+        return this.students;
+    }
+    public void addStudent(Student student){
+        if(!isStudentEnrolled(student) && student.getYearLevel() == this.yearLevel){
+            this.students.add(student);
+        }
+    }
+    public void removeStudent(Student student){
+        this.students.remove(student);
+    }
+    public boolean isStudentEnrolled(Student student){
+        if(students.contains(student)){
+            return true;
+        }
+        return false;
+    }
+    public String toString(){
+        return "ClassSection{subject='" + subject + "', capacity=" + capacity + ", yearLevel=" + yearLevel + ", students=" + students + "}";
+    }
+
 
 
 }
--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,59 @@
 public class Student {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private int yearLevel;
+    private static int studentCount;
+    public Student(String firstName, String lastName, int age, int yearLevel){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.yearLevel = yearLevel;
+
+    }
+    public int getId(){
+        return id;
+    }
+    public void setId(int Id){
+        this.id = Id;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public int getYearLevel(){
+        return yearLevel;
+    }
+    public void setYearLevel(int yearLevel){
+        this.yearLevel = yearLevel;
+    }
+    public int getStudentCount(){
+        return studentCount;
+    }
+    public boolean equals(Student student){
+        if(firstName.equals(student.firstName) && lastName.equals(student.lastName) && age == student.age && yearLevel == yearLevel){
+            return true;
+        }
+        return false;
+    }
+    public String toString(){
+        return "Student{id=77" + ", firstName='" + this.firstName+ "', lastName='" + this.lastName + "', age=" + this.age + ", yearLevel=" + this.yearLevel +"}";
+    }
 
 
 }
